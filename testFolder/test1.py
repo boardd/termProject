b716653ae3743191deff675779e98b56427bab8f
Tony <74562881+tonytao728@users.noreply.github.com>
2020-11-19 00:18:30 -0500

    commit on "main"

--- testFolder/test1.py
+++ testFolder/test1.py
@@ -1,9 +1,23 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("testImage.jpeg")
-print(img.shape)
-imgResized = cv2.resize(img, (1680,1050))
-cv2.imshow("Test Image", imgResized)
+# BGR not RGB
+blue = (255,0,0)
+green = (0, 255, 0)
+red = (0,0,255)
 
+# image preprocessing
+img = cv2.imread("testImage.jpeg")
+img = cv2.resize(img, (1120,700))
+greyImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# face detection code
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faces = faceCascade.detectMultiScale(greyImg, scaleFactor=1.2, minNeighbors=3)
+for rec in faces:
+    x, y, w, h = rec
+    cv2.putText(img, "Face", (x,y-10), fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 1, color = green)
+    cv2.rectangle(img, (x,y), (x+w,y+h), color = green, thickness = 1)
+
+cv2.imshow("Test Image", img)
 cv2.waitKey(0)
---
+++ testFolder/test2.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+# BGR not RGB
+blue = (255,0,0)
+green = (0, 255, 0)
+red = (0,0,255)
+
+
+# image preprocessing
+frameWidth = 720
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+# width
+cap.set(3,frameWidth)
+# height
+cap.set(4,frameHeight)
+# brightness
+cap.set(10,100)
+
+# face detection code
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+while True:
+    success, frame = cap.read()
+    greyFrame = cv2.cvtColor(webcam, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(greyFrame, scaleFactor=1.1, minNeighbors=2)
+    for rec in faces:
+        x, y, w, h = rec
+        cv2.putText((frame), "Face", (x,y-10), fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 1, color = green)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), color = red, thickness = 1)
+
+    cv2.imshow("webcam", frame)
+
+    if cv2.waitKey(1) and 0xFF == ord("q"):
+        break
